1d10febcdffb58bbcc66593b9818fc814ca93173
Chr0nicT <46724628+Chr0nicT@users.noreply.github.com>
2019-04-20 20:33:24 -0400

    Cydia and Jailbreak Status

---
+++ README.md
@@ -0,0 +1,19 @@
+# Tw3lve
+iOS 12-12.1.2 Jailbreak (WORK IN PROGRESS DO NOT USE)
+
+
+
+## DONE:
+* Root     
+* Sandbox Escape    
+* TFP0      
+* Remap TFP Set HSP      
+* Remount RootFS
+* GUI
+* Other jailbreak detection to avoid issues  
+
+## TODO:
+* Install Bootstrap
+* Tweak injection
+* Fix Bugs (Performance)    
+
---
+++ tw3lve/Tw3lveSettings.m
@@ -0,0 +1,62 @@
+//
+//  Tw3lveSettings.m
+//  tw3lve
+//
+//  Created by Tanay Findley on 4/19/19.
+//  Copyright © 2019 Tanay Findley. All rights reserved.
+//
+
+#import "Tw3lveSettings.h"
+#include "common.h"
+
+@interface Tw3lveSettings () {
+    IBOutlet UISwitch *restoreFSSwitch;
+    IBOutlet UISwitch *loadTweaksSwitch;
+    IBOutlet UISwitch *sileoSwitch;
+    IBOutlet UISegmentedControl *exploitTypeBar;
+}
+
+@end
+
+@implementation Tw3lveSettings
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self fixData];
+}
+
+- (void)fixData {
+    [exploitTypeBar setSelectedSegmentIndex:[[NSUserDefaults standardUserDefaults] integerForKey:EXPLOITTYPE]];
+    [restoreFSSwitch setOn:[[NSUserDefaults standardUserDefaults] boolForKey:RESTORE_FS]];
+    [loadTweaksSwitch setOn:[[NSUserDefaults standardUserDefaults] boolForKey:LOAD_TWEAKS]];
+    //[sileoSwitch setOn:[[NSUserDefaults standardUserDefaults] boolForKey:INSTALL_SILEO]];
+}
+
+- (IBAction)rootFSChanges:(id)sender {
+    [[NSUserDefaults standardUserDefaults] setBool:[restoreFSSwitch isOn] forKey:RESTORE_FS];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    [self fixData];
+}
+
+
+- (IBAction)loadTweaksChanges:(id)sender {
+    [[NSUserDefaults standardUserDefaults] setBool:[loadTweaksSwitch isOn] forKey:LOAD_TWEAKS];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    [self fixData];
+}
+
+- (IBAction)installSileoChanges:(id)sender {
+    [[NSUserDefaults standardUserDefaults] setBool:[sileoSwitch isOn] forKey:INSTALL_SILEO];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    [self fixData];
+}
+
+- (IBAction)exploitTypeChanges:(id)sender {
+    [[NSUserDefaults standardUserDefaults] setInteger:exploitTypeBar.selectedSegmentIndex forKey:EXPLOITTYPE];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    [self fixData];
+}
+
+
+
+@end
